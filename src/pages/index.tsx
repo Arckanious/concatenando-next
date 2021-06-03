20ed1f3616ced420d570c047d394b7bbfede8f94
William Osteimer Cardoso dos Anjos <wosteimer1@gmail.com>
2021-06-03 20:55:23 -0300

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Concatenando</title>
+      </Head>
+
+      <h1>Hello Next</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+  padding:0;
+  margin:0;
+  vertical-align:baseline;
+  list-style:none;
+  border:0
+  }
+`
