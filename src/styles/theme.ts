91de3c07842522134ca945203059031aeec13e17
William Osteimer Cardoso dos Anjos <wosteimer1@gmail.com>
2021-06-03 20:55:23 -0300

    initial setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#802392',
+    secondary: '#f7934c',
+    white: '#fbf5f3',
+    black: '0f0e0e'
+  }
+}
+
+export default theme
